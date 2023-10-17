d2014f22e6e777299b5d27217c8b4417948da854
Julian Kimmig <julian.kimmig@gmx.net>
2023-10-17 09:22:51 +0200

    logger format

--- polyfingerprints/logger.py
+++ polyfingerprints/logger.py
@@ -1,3 +1,10 @@
 import logging
 
 PFPLOGGER = logging.getLogger("polyfingerprints")
+PFPLOGGER.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+PFPLOGGER.addHandler(handler)
