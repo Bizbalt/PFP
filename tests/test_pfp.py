653f5b04c1355b705a8042e145f797bf76145cb7
Bizbalt <yannik.koester@uni-jena.de>
2023-11-29 15:15:21 +0100

    :white_check_mark: fixed wrong test assignment for fp length

--- tests/test_pfp.py
+++ tests/test_pfp.py
@@ -11,9 +11,9 @@ class TestCreatePfp(unittest.TestCase):
 
         pfp = create_pfp(repeating_units, mol_weight)
         # Default settings should produce a Polyfingerprint with a length of:
-        # 2048 + (2 * 2048) * 1 = 6144
+        # 2048 + (2 * 2048) * 2 = 10240
         self.assertIsInstance(pfp, np.ndarray)
-        self.assertEqual(len(pfp), 6144)
+        self.assertEqual(len(pfp), 10240)
 
     def test_create_pfp_with_enhanced_functions(self):
         from polyfingerprints.fingerprints import create_RDKFingerprint
